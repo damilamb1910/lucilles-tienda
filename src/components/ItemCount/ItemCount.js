8173327f96d257a16aca67930372c172db737c0b
Damian Lamberti <damilamb1910@gmail.com>
2022-09-08 00:21:53 -0300

    commit add

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import "./ItemCount.css"
+
+
+const ItemCount=({stock,funcion})=>{
+
+
+
+    const agregarCarrito=()=>{
+        
+        if(contador<=stock && contador >=1){
+            
+        
+            funcion(contador)
+            setContador(1)
+        }
+            
+     
+        
+    }
+
+
+
+
+    
+    const[contador,setContador]=useState(1)
+    const sumar=()=>{
+
+    setContador(contador + 1)
+}
+
+
+    
+    const restar=()=>{
+        if(contador>1){
+            setContador(contador - 1)
+        }
+        
+    }
+    return(
+        <>
+        <div className="contenedorItemCount">
+<p>nombre de producto</p>
+<div className="contenedorSumarRestar">
+<button className="btn" onClick={restar}>-</button>
+<h5>{contador}</h5>
+<button className="btn" onClick={sumar}>+</button>
+
+</div>
+<button className="btnAdd" onClick={agregarCarrito}>Agregar al carrito</button>
+
+        </div>
+
+        </>
+        
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,17 @@
+import ItemCount from "../ItemCount/ItemCount"
 import "./ItemListContainer.css"
 
 const ItemListContainer=({greeting})=>{
-    return(
-        <h1>{greeting}</h1>
+const onAdd=(contador)=>{
+    console.log(contador)
+}
+
+
+    return(<>
+    <h1>{greeting}</h1>
+    <ItemCount stock={10} inicial={1} funcion={onAdd}/>
+    </>
+        
     )
 }
 
